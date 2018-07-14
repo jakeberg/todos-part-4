fc670e50f98a9e4dec91d67ad0694aa3a78f1daf
Jake Berg <jacobvictorberg@gmail.com>
2018-07-14 15:35:12 -0400

    nuked repo

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { reducers } from './reducers';
+
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() )
+
+ReactDOM.render(
+    <BrowserRouter>
+    <Provider store={ store }>
+        <App />
+    </Provider>
+    </BrowserRouter>,
+    document.getElementById('root')
+);
+registerServiceWorker();
